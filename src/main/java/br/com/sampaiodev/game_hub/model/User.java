01062484e4650b1a3388e2eb4b257ee9715da0b3
Fernanda <dev.sampaio7@proton.me>
2026-08-12 12:29:20 -0300

    Refatorando a entidade User

--- src/main/java/br/com/sampaiodev/game_hub/model/User.java
+++ src/main/java/br/com/sampaiodev/game_hub/model/User.java
@@ -1,34 +1,41 @@
 package br.com.sampaiodev.game_hub.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.envers.Audited;
 
 @Entity
 @Audited
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
-@Table(name = "User")
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "Id")
     private Long id;
 
-    @Column(name = "Username")
+    @Column(name = "username", nullable = false, unique = true, length = 50)
     private String username;
 
-    @Column(name = "Email")
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
-    @Column(name = "Password")
+    @Column(name = "password", nullable = false)
     private String password;
 
-    @Column(name = "Bio")
+    @Column(name = "bio", length = 500)
     private String bio;
 
-    @Column(name = "ProfilePictureURL")
-    private String profilePictureURL;
+    @Column(name = "profile_picture_url")
+    private String profilePictureUrl;
+
+    @Column(name = "following_count")
+    private Integer followingCount = 0;
+
+    @Column(name = "followers_count")
+    private Integer followersCount = 0;
 }
