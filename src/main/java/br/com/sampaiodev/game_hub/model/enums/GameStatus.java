0fd0811ec39f61875f31b30b3d1444c7ea7c090e
Fernanda <dev.sampaio7@proton.me>
2026-08-12 11:36:55 -0300

    Adicionado e preenchido os enums do 'status do jogo(GameStatus)' e 'tipo de lista(ListType)'

---
+++ src/main/java/br/com/sampaiodev/game_hub/model/enums/GameStatus.java
@@ -0,0 +1,9 @@
+package br.com.sampaiodev.game_hub.model.enums;
+
+public enum GameStatus {
+    PLAYING,
+    COMPLETED,
+    MASTERE,
+    PAUSED,
+    ABANDONED
+}
---
+++ src/main/java/br/com/sampaiodev/game_hub/model/enums/ListType.java
@@ -0,0 +1,7 @@
+package br.com.sampaiodev.game_hub.model.enums;
+
+public enum ListType {
+    FAVORITE,
+    WISHLIST,
+    CUSTOM
+}
