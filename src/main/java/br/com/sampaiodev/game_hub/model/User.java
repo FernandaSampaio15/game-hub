9b6fffa6c116595fb3e4205fed99554b0fbe9720
Fernanda <dev.sampaio7@proton.me>
2026-08-12 11:49:16 -0300

    Configurada a classe/entidade User

--- src/main/java/br/com/sampaiodev/game_hub/model/User.java
+++ src/main/java/br/com/sampaiodev/game_hub/model/User.java
@@ -1,4 +1,34 @@
 package br.com.sampaiodev.game_hub.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.envers.Audited;
+
+@Entity
+@Audited
+@Data
+@NoArgsConstructor
+@Table(name = "User")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
+    private Long id;
+
+    @Column(name = "Username")
+    private String username;
+
+    @Column(name = "Email")
+    private String email;
+
+    @Column(name = "Password")
+    private String password;
+
+    @Column(name = "Bio")
+    private String bio;
+
+    @Column(name = "ProfilePictureURL")
+    private String profilePictureURL;
 }
